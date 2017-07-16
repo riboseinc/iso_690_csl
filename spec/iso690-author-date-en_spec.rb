cdbbb6758de0307cfe285a1e19729f2eb8694379
Paolo Brasolin <paolo@leanbit.it>
2017-07-16 16:39:34 +0200

    add a couple of sample references

--- spec/iso690-author-date-en_spec.rb
+++ spec/iso690-author-date-en_spec.rb
@@ -4,4 +4,6 @@ require 'spec_helper'
 
 describe :"iso690-author-date-en" do
   it { expect( bibliographic_entry_for('Nobody06') ).to eq('JR, Nobody, 2006. _My Article_. 2006. ') }
+  it { expect( bibliographic_entry_for('ISO13586') ).to eq('') }
+  it { expect( bibliographic_entry_for('DPA1984') ).to eq('') }
 end
